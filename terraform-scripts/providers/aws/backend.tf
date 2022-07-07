6f27798c73f09ad66390146560daee7fe034f596
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-07-07 14:34:59 -0500

    Update backend.tf

--- terraform-scripts/providers/aws/backend.tf
+++ terraform-scripts/providers/aws/backend.tf
@@ -1,17 +1,17 @@
 terraform {
 
   backend "s3" {
-    profile = "poc"
+    #profile = "poc"
     region  = "us-east-2"
     bucket          = "containers-terraform-state-storage"
-    dynamodb_table  = "terraform-state-lock"
+    dynamodb_table  = "great-name-terraform-state-2222"
     key     = "service/ansible/terraform.tfstate"
     encrypt = true
   }
 }
 
 provider "aws" {
-  profile = "poc"
+  #profile = "poc"
   //region  = "us-east-2"
   region  = "${var.aws_region}"
 }
